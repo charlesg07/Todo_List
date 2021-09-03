113f3b0f86b5eca49dba76b9a382d397d6295697
Charles Guerrero <charles.allfiction@gmail.com>
2021-09-03 10:44:23 +0800

    Task Section done

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style={styles.items}>
+          {}
+
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff', 
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  }, 
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  }, 
+  items: {}, 
 });
